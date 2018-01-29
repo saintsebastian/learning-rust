a102c4a5e508a0e3e836803263fedad27ddfbdba
saintsebastian <tilitidam@gmail.com>
2018-01-29 20:11:19 +0100

    make current immutable and move it into the loop

--- fib/src/main.rs
+++ fib/src/main.rs
@@ -5,9 +5,8 @@ fn main() {
 fn fib_function(x: i32) {
     let mut penultimate: i32 = 0;
     let mut previous: i32 = 1;
-    let mut current: i32;
     for _number in 1..x {
-        current = previous + penultimate;
+        let current = previous + penultimate;
         println!("{}!", current);
         penultimate = previous;
         previous = current;
